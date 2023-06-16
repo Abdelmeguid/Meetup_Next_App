0a8e8fd52f1eca9c3b7c44424809ff6dfd49b39e
BEDO <siegen567@gmail.com>
2023-06-16 23:19:31 +0300

    Initial commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,28 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://ahmed:qwerty666666@cluster0.ztcuetb.mongodb.net/?retryWrites=true&w=majority",
+      { useUnifiedTopology: true, useNewUrlParser: true }
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,42 @@
+// our-domain.com/new-meetup
+import { Fragment } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    //BZH in below line ( /api/new-meetup ) refer to api or we put here the url and key and token of this API
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
+}
+
+export default NewMeetupPage;
